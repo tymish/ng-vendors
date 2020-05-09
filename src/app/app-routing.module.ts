d2e9fe3304d63947366f0acaa0ba246ea706baa1
Hubert Lin <linhub15@gmail.com>
2020-05-09 17:39:30 -0600

    add components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,31 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { InvoicesComponent } from './invoices/invoices.component';
+import { MyAccountComponent } from './my-account/my-account.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+  },
+  {
+    path: 'my-account',
+    component: MyAccountComponent,
+  },
+  {
+    path: 'invoices',
+    component: InvoicesComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { InvoicesComponent } from './invoices/invoices.component';
+import { HomeComponent } from './home/home.component';
+import { MyAccountComponent } from './my-account/my-account.component';
+import { LoginComponent } from './auth/login/login.component';
+import { LogoutComponent } from './auth/logout/logout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    InvoicesComponent,
+    HomeComponent,
+    MyAccountComponent,
+    LoginComponent,
+    LogoutComponent
   ],
   imports: [
     BrowserModule,
