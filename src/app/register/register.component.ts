f8daf5bc386c66357c4c76b8c78d4bfcde71a4ca
Hubert Lin <linhub15@gmail.com>
2020-10-11 22:12:36 -0600

    implement register vendor form

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,29 +1,77 @@
 import { Component, OnInit } from '@angular/core';
 import { VendorsService } from '../core/api/services';
 import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Vendor } from '../core/api/models';
+import {
+  FormControl,
+  FormGroup,
+  ValidationErrors,
+  ValidatorFn,
+} from '@angular/forms';
+import { tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent implements OnInit {
   private vendorId: string;
+  vendor$: Observable<Vendor>;
+
+  private passwordMismatch: ValidatorFn = (
+    control: FormGroup
+  ): ValidationErrors | null => {
+    const password = control.get('password');
+    const confirmPassword = control.get('confirmPassword');
+    return password &&
+      confirmPassword &&
+      password.value === confirmPassword.value
+      ? null
+      : { passwordMismatch: true };
+  };
+  vendorForm = new FormGroup({
+    email: new FormControl(),
+    mobilePhone: new FormControl(),
+    password: new FormControl(),
+    confirmPassword: new FormControl(),
+  }, {validators: [this.passwordMismatch] });
   hide = true;
+
+  get mobilePhone() {
+    return this.vendorForm.get('mobilePhone');
+  }
+  get password() {
+    return this.vendorForm.get('password');
+  }
+
   constructor(
     private readonly route: ActivatedRoute,
-    private readonly vendors: VendorsService) { }
+    private readonly vendors: VendorsService
+  ) {}
 
   ngOnInit(): void {
-    this.vendorId = this.route.snapshot.params['id']
+    this.vendorId = this.route.snapshot.params['id'];
+    this.vendor$ = this.vendors
+      .getVendor({ id: this.vendorId })
+      .pipe(tap((vendor) => this.vendorForm.patchValue(vendor)));
   }
 
   register() {
-    // Validate password matches and all that
-    this.vendors.registerVendor({body: {
-      mobilePhone: '7806665555',
-      password: 'password',
-      vendorId: this.vendorId
-    }}).subscribe();
+    if (this.vendorForm.invalid) {
+      alert('invalid');
+      return;
+    }
+
+    this.vendors
+      .registerVendor({
+        body: {
+          vendorId: this.vendorId,
+          mobilePhone: this.mobilePhone.value,
+          password: this.password.value,
+        },
+      })
+      .subscribe();
   }
 }
