654530d104a4add13e3c13f777ab9981664d045d
Hubert Lin <linhub15@gmail.com>
2020-10-17 23:37:32 -0600

    invoices displays invoices

--- src/app/invoices/invoices.component.ts
+++ src/app/invoices/invoices.component.ts
@@ -1,16 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Invoice } from '../core/api/models';
+import { VendorsService } from '../core/api/services';
 
 @Component({
   selector: 'app-invoices',
   templateUrl: './invoices.component.html',
-  styleUrls: ['./invoices.component.scss']
+  styleUrls: ['./invoices.component.scss'],
 })
 export class InvoicesComponent implements OnInit {
+  constructor(private readonly vendors: VendorsService) {}
 
-  constructor() { }
+  invoices$: Observable<Invoice[]>;
 
   ngOnInit(): void {
-    // load the invoices for the vendor
+    this.invoices$ = this.vendors.listVendorInvoices({
+      id: '014d9f31-6a28-4f5f-abfa-9f5ab04f93cc',
+    });
   }
-
 }
