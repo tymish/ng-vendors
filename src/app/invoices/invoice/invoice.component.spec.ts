160b7827eff02484fba4107ca558ec1b3957ae6c
Hubert Lin <linhub15@gmail.com>
2020-11-15 11:12:00 -0700

    update angular 11

--- src/app/invoices/invoice/invoice.component.spec.ts
+++ src/app/invoices/invoice/invoice.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { InvoiceComponent } from './invoice.component';
 
@@ -6,7 +6,7 @@ describe('InvoiceComponent', () => {
   let component: InvoiceComponent;
   let fixture: ComponentFixture<InvoiceComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ InvoiceComponent ]
     })
