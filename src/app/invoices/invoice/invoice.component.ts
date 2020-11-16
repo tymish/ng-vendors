02a74f9c5d83324a7d7dbb0d311f1aa4a2a5e0a0
Hubert Lin <linhub15@gmail.com>
2020-11-15 18:00:31 -0700

    display invoice as a table

--- src/app/invoices/invoice/invoice.component.ts
+++ src/app/invoices/invoice/invoice.component.ts
@@ -1,8 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
-import { InvoiceDto } from 'src/app/core/api/models';
+import { InvoiceDto, TimeEntryDto } from 'src/app/core/api/models';
 import { InvoicesService } from 'src/app/core/api/services';
+
+interface Row {
+  date: string;
+  hours: number;
+  time: string;
+  comments: string;
+  amount: number;
+}
 
 @Component({
   selector: 'app-invoice',
@@ -18,8 +26,52 @@ export class InvoiceComponent implements OnInit {
 
   invoice$: Observable<InvoiceDto>;
 
+  displayedColumns: string[] = [
+    'date',
+    'hours',
+    'time',
+    'comments',
+    'amount'
+  ];
+
   ngOnInit(): void {
     const invoiceId = this.route.snapshot.params['id'];
     this.invoice$ = this.invoices.getInvoiceById({id: invoiceId });
+  }
+
+  timeEntries(invoice: InvoiceDto): Row[] {
+    return invoice.timeEntryDtos.map((timeEntry) => {
+      return this.transform(timeEntry, invoice.vendorDto.hourlyPay);
+    })
+  }
+
+  private transform(timeEntry: TimeEntryDto, pay: number): Row {
+    return {
+      date: this.parseDate(new Date(timeEntry.start)),
+      hours: timeEntry.totalHours,
+      time: this.parseTime(timeEntry.start, timeEntry.end),
+      comments: timeEntry.comments,
+      amount: timeEntry.totalHours * pay
+    }
+  }
+
+  private parseDate(date: Date): string {
+    return date.toLocaleDateString('en-ca', {
+      month: 'short', day: 'numeric'
+    });
+  }
+
+  private parseTime(start: string, end: string): string {
+    const startTime = new Date(start)
+      .toLocaleTimeString(navigator.language, {
+        hour: '2-digit',
+        minute: '2-digit'
+      });
+    const endTime = new Date(end)
+      .toLocaleTimeString(navigator.language, {
+        hour: '2-digit',
+        minute: '2-digit'
+      });
+    return `${startTime} - ${endTime}`;
   }
 }
